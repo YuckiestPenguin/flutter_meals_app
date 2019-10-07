1559ab183734f4d781ca9f84c79ca79f6701bbfe
Vico Melo <vinicomelo@gmail.com>
2019-10-07 19:07:01 -0400

    transition to page with title in app bar

--- lib/categories_meals_screen.dart
+++ lib/categories_meals_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoriesMealsScreen(this.categoryId, this.categoryTitle);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('Recipes for this category'),
