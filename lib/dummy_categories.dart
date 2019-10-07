564e3d0bcde4da1e220fe3983aa568473bb3f118
Vico Melo <vinicomelo@gmail.com>
2019-10-07 14:31:29 -0400

    additional stuff

--- lib/dummy_categories.dart
+++ lib/dummy_categories.dart
@@ -22,4 +22,35 @@ const DUMMY_CATEGORIES = const [
     title: 'Quick & Easy',
     color: Colors.yellow,
   ),
+    Category(
+    id: 'c5',
+    title: 'Stuff',
+    color: Colors.redAccent,
+  ),
+    Category(
+    id: 'c6',
+    title: 'Other Stuff',
+    color: Colors.teal,
+  ),
+    Category(
+    id: 'c7',
+    title: 'Desserts',
+    color: Colors.purple,
+  ),
+    Category(
+    id: 'c8',
+    title: 'Latin',
+    color: Colors.cyan,
+  ),
+    Category(
+    id: 'c9',
+    title: 'Asian',
+    color: Colors.brown,
+  ),
+    Category(
+    id: 'c10',
+    title: 'Boring Stuff',
+    color: Colors.indigo,
+  ),
+
 ];
