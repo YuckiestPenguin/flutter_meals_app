69468873957c03cf4546d4bd948aab976552f935
Vico Melo <vinicomelo@gmail.com>
2019-10-07 19:34:53 -0400

    route named differently

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       ),
       home: CategoriesScreen(),
       routes: {
-        '/categories-meals': (ctx) => CategoriesMealsScreen(),
+        CategoriesMealsScreen.routeName: (ctx) => CategoriesMealsScreen(),
       },
     );
   }
