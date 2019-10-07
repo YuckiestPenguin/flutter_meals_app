f8446f8b0f52d58b9732458901e587ce4714aedc
Vico Melo <vinicomelo@gmail.com>
2019-10-07 14:26:34 -0400

    skeleton

---
+++ lib/dummy_categories.dart
@@ -0,0 +1,25 @@
+import './models/category.dart';
+import 'package:flutter/material.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c2',
+    title: 'French',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Burgers',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Quick & Easy',
+    color: Colors.yellow,
+  ),
+];
