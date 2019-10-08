e2521c97056840e314058f33c04ede7d70369727
Vico Melo <vinicomelo@gmail.com>
2019-10-07 20:48:24 -0400

    format

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,11 +6,12 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text('Meals App')),
+      appBar: AppBar(title: const Text('Meals App')),
       body: GridView(
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.id,catData.title, catData.color))
+            .map((catData) =>
+                CategoryItem(catData.id, catData.title, catData.color))
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
